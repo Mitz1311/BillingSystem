a31a578474d543e5070a518ba1f3b983a8a1a368
Mitz1311 <69730988+Mitz1311@users.noreply.github.com>
2021-07-03 17:35:16 +0530

    BS added sucessfully

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Project;
+;
+import java.sql.*;
+
+/**
+ * @author KARAN
+ */
+public class ConnectionProvider {
+   public static Connection getCon()
+    {
+        try
+        {
+             Class.forName("com.mysql.jdbc.Driver");
+          Connection  con=DriverManager.getConnection("jdbc:mysql://localhost:3306/billing","root","");
+         
+          return con;
+          
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+        
+    }
+   /* private Connection con;
+    private Statement st;
+    private ResultSet rs;
+    public ConnectionProvider()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/billing","root","");
+            st=con.createStatement();
+            
+        }
+        catch(Exception ex)
+        {
+            System.out.println("error"+ex);
+        }
+    }*/
+
+    
+
+   
+    
+}
